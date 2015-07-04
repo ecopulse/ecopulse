ae4aedb9f6245edeb03ac68b3a078a0591ed561d
Michael Dilley <michael.dilley@gmail.com>
2015-07-04 14:52:51 +1000

    Only show 1 chart at a time

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,15 +17,44 @@ angular.module('ecopulse')
 
     $scope.datasets = [];
 
+    var redrawTargetLine = function(chart, yPoint) {
+      // Get rid of the old target line
+      if ($scope.targetLine) { $scope.targetLine.destroy(); }
+
+      // Draw the new target line
+      $scope.targetLine = chart.renderer
+        .path(['M', yPoint, chart.plotTop, 'V', chart.plotSizeY + chart.plotTop])
+        .attr({
+          'stroke-width': 1,
+          stroke: 'silver',
+          dashstyle: 'dash'
+      });
+
+      // Draw the new line on the chart object
+      $scope.targetLine.add();
+    }
+
+    $scope.updateTargetDate = function(ev) {
+      // Current scope is the chart
+      redrawTargetLine(this, ev.chartX);
+
+      // Update the global target date
+      $scope.targetDate = this.xAxis[0].toValue(ev.chartX);
+    };
+
     $scope.getData = function () {
       $scope.highchartsNG.loading = true;
 
+      /* Reset datasets */
       $scope.datasets = [];
+      $scope.targetDate = Date.now();
+
+      /* Clear chart */
+      $scope.highchartsNG.series = [];
 
       _.each([1,2,3], function(prop_type) {
         _.each([1,2,3], function(measure) {
           Query.process($scope.dataset, prop_type, measure, $scope.start_date).then(function(result) {
-            console.debug(result);
             result = Transform.convert('stat.ABS', result.data);
 
             $scope.datasets.push({
@@ -35,11 +64,11 @@ angular.module('ecopulse')
               icon: 'home',
               data: result
             });
-
-            $scope.updateChart(
-              result,
-              prop_type_strings[prop_type] + ' ' + measure_strings[measure]
-            );
+            //
+            // $scope.updateChart(
+            //   result,
+            //   prop_type_strings[prop_type] + ' ' + measure_strings[measure]
+            // );
           });
         });
       });
@@ -47,29 +76,33 @@ angular.module('ecopulse')
       $scope.highchartsNG.loading = false;
     };
 
-    $scope.updateChart = function(vals, title) {
-      $scope.highchartsNG.series.push({
-        data: vals,
-        name: title
-      })
-    }
-
-    $scope.clearChart = function () {
+    $scope.chartSeries = function(datasetId) {
+      /* Clear chart */
       $scope.highchartsNG.series = [];
+
+      $scope.currentGraphedDataset = _.find($scope.datasets, function(set) {
+        return set.id == datasetId;
+      });
+
+      $scope.highchartsNG.title.text = $scope.currentGraphedDataset.name;
+      $scope.highchartsNG.series.push($scope.currentGraphedDataset);
     }
 
     $scope.highchartsNG = {
       options: {
         chart: {
-          type: 'line'
-        },
-        tooltip: {
-          formatter: function() {
-            var quarter = Math.ceil((new Date(this.x).getMonth() + 1) / 3);
-            var s = '<b>' + Highcharts.dateFormat('Q' + quarter + ' %Y' , this.x) + '</b>';
-            s += '<br/>' + this.series.name + ': ' + this.y;
-            return s;
+          type: 'line',
+          events:{
+            click: $scope.updateTargetDate
           }
+        },
+        plotOptions: {
+            line: {
+                dataLabels: {
+                    enabled: true
+                },
+                enableMouseTracking: false
+            }
         },
         xAxis: {
           type: 'datetime',
@@ -91,7 +124,10 @@ angular.module('ecopulse')
       },
       series: [],
       title: {
-        text: $scope.dataset
+        text: "Our Economic Heartbeat"
+      },
+      legend: {
+        enabled: false
       },
       loading: false
     }
